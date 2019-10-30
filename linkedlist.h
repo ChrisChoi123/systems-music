932b328e7537930d138631c87d70ccf1f77a387e
Chris Choi <mchoi00@stuy.edu>
2019-10-30 10:13:01 -0400

    added stuff

--- linkedlist.h
+++ linkedlist.h
@@ -2,12 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct node {
-  int num;
-  struct node *next;
+struct song_node{
+  char name[100];
+  char artist[100];
+  struct song_node *next;
 };
 
-void print_list(struct node * n);
-struct node * insert_front(struct node * n, int i);
-struct node * free_list(struct node * n);
-struct node * remove_num(struct node *front, int data);
+//struct node {
+  //int num;
+  //struct node *next;
+//};
+
+//sam
+void print_list(struct song_node * l);
+struct song_node * insert_front(struct song_node * l, char n,char a);
+struct song_node * insert_ordered(struct song_node * l, char n, char a);
+struct song_node * find_node(struct song_node * l, char n, char a);
+//chris
+struct song_node * find_first_song(struct song_node * l, char a);
+struct song_node * rand_song(struct song_node * l);
+struct song_node * remove_song(struct song_node * l, struct song_node * rem);
+struct song_node * free_list(struct song_node * l);
