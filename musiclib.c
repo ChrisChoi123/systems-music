533353c35594b683cb969b8c2ff910c0f32746da
Chris Choi <mchoi00@stuy.edu>
2019-11-01 10:59:58 -0400

    wrote print_artist, but probably doesn't work, considering my luck

--- musiclib.c
+++ musiclib.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "musiclib.h"
+
+void print_artist(struct song_node * l, char * a) {
+  if (a[0] < 97 || a[0] > 122) {
+    struct song_node point = find_first_song(l[0], a);
+    while (strcmp(point->next->artist,a) == 0) {
+      printf(" %s: %s |", point->artist, point->name);
+      point = point->next;
+    }
+  }
+  else {
+    int idx = a[0] - 96;
+    struct song_node point = find_first_song(l[idx], a);
+    while (strcmp(point->next->artist,a) == 0) {
+      printf(" %s: %s |", point->artist, point->name);
+      point = point->next;
+    }
+  }
+  printf("\n");
+}
--- musiclib.h
+++ musiclib.h
@@ -8,8 +8,8 @@ struct song_node * search_song(struct song_node * l, char * n, char * a);
 struct song_node * search_artist(struct song_node * l, char * n, char * a);
 struct song_node * print_letter(struct song_node * l, char * a);
 //chris
-struct song_node * print_artist(struct song_node * l);
-struct song_node * print_lib(struct song_node * l, struct song_node * rem);
-struct song_node * shuffle(struct song_node * l);
+void print_artist(struct song_node * l, char * a);
+void print_lib(struct song_node * l);
+void shuffle(struct song_node * l);
 struct song_node * delete_song(struct song_node * l, struct song_node * rem);
 struct song_node * clear_lib(struct song_node * l);
