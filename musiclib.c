4c363bfc7e7f56a38c2d89c47bdbdc2bb57580ea
Chris Choi <mchoi00@stuy.edu>
2019-11-02 12:31:40 -0400

    finished print_artist and print_lib

--- musiclib.c
+++ musiclib.c
@@ -6,18 +6,20 @@
 void print_artist(struct song_node * l, char * a) {
   if (a[0] < 97 || a[0] > 122) {
     struct song_node point = find_first_song(l[0], a);
-    while (strcmp(point->next->artist,a) == 0) {
-      printf(" %s: %s |", point->artist, point->name);
-      point = point->next;
-    }
   }
   else {
     int idx = a[0] - 96;
     struct song_node point = find_first_song(l[idx], a);
-    while (strcmp(point->next->artist,a) == 0) {
-      printf(" %s: %s |", point->artist, point->name);
-      point = point->next;
-    }
+  }
+  while (strcmp(point->artist,a) == 0) {
+    printf(" %s: %s |", point->artist, point->name);
+    point = point->next;
   }
   printf("\n");
+}
+
+void print_lb(struct song_node * l) {
+  for (int i = 0, i < 27; i++) {
+    print_list(l[i]);
+  }
 }
