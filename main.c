dd2afba6f82f53e26873c56486117ae9d2db40a8
SamuelFang <47552568+SamuelFang@users.noreply.github.com>
2019-11-04 00:56:40 -0500

    finished main, still needs fixing

--- main.c
+++ main.c
@@ -67,6 +67,62 @@ int main() {
   for (int i = 0; i < 27; i++) {
     table[i] = NULL;
   }
-
+  printf("Testing add_song and print_lib:\n");
+  printf("Adding a whole bunch of songs:\n");
+  table = add_song(table,"celestial elixir","haken" );
+  table = add_song(table,"dystopian overture","dream theater" );
+  table = add_song(table,"la villa strangiato","rush" );
+  table = add_song(table,"cygnus x-1","rush" );
+  table = add_song(table,"paranoid android","radiohead" );
+  table = add_song(table,"visions","haken" );
+  table = add_song(table,"the remembering","yes" );
+  table = add_song(table, "the truth will set you free", "the flower kings");
+  table = add_song(table, "stardust we are", "the flower kings");
+  table = add_song(table, "garden of dreams", "the flower kings");
+  table = add_song(table,"the architect","haken" );
+  table = add_song(table, "octavarium", "dream theater");
+  table = add_song(table, "renai circulation", "namirin");
+  print_lib(table);
+  printf("====================================\n");
+  printf("Testing print_letter:\n");
+  print_letter(table, "r");
+  printf("====================================\n");
+  printf("Testing print_artist:\n");
+  print_artist(table, "rush");
+  printf("====================================\n");
+  printf("Testing search_song:\n");
+  printf("looking for the remembering by yes\n");
+  print_node(search_song(table,"the remembering", "yes"));
+  printf("looking for the architect by haken\n");
+  print_node(search_song(table,"the architect", "haken"));
+  printf("looking for kaleidoscope by transatlantic\n");
+  print_node(search_song(table,"kaleidoscope", "transatlantic"));
+  printf("====================================\n");
+  printf("Testing search_artist:\n");
+  printf("looking for rush\n");
+  print_list(search_artist(table,"rush"));
+  printf("looking for the flower kings\n");
+  print_list(search_artist(table,"the flower kings"));
+  printf("looking for spock's beard\n");
+  print_list(search_artist(table,"spock's beard"));
+  printf("====================================\n");
+  printf("Testing shuffle:\n");
+  shuffle(table, 5)
+  printf("====================================\n");
+  printf("Testing delete_song:\n");
+  printf("Deleting visions by haken:\n");
+  table = delete_song(table, search_song(table,"visions", "haken"));
+  print_lib(table);
+  printf("Removing garden of dreams by the flower kings:\n");
+  table = delete_song(table, search_song(table,"garden of dreams", "the flower kings"));
+  print_lib(table);
+  printf("Removing crystallised by haken (list should not change):\n");
+  table = delete_song(table, search_song(table,"visions", "haken"));
+  print_lib(table);
+  printf("====================================\n");
+  printf("Testing clear_lib:\n");
+  table = clear_lib(table);
+  printf("List after clear_lib:");
+  print_lib(table);
   return 0;
 }
